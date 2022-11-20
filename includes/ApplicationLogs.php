341c864326960e1a102325a534296ed503818359
Dennis Ploetner <re@lloc.de>
2022-11-20 18:45:16 +0100

    Logging added

--- includes/ApplicationLogs.php
+++ includes/ApplicationLogs.php
@@ -15,11 +15,11 @@ class ApplicationLogs {
 	public function __construct() {
 		$formatter = new JsonFormatter();
 
-		$stream_handler = new StreamHandler("php://stdout", Logger::DEBUG );
+		$stream_handler = new StreamHandler( "php://stdout", Logger::DEBUG );
 		$stream_handler->setFormatter( $formatter );
 
-		$this->logger = new Logger(self::NAME );
-		$this->logger->pushHandler($stream_handler);
+		$this->logger = new Logger( self::NAME );
+		$this->logger->pushHandler( $stream_handler );
 	}
 
 	/**
@@ -28,7 +28,7 @@ class ApplicationLogs {
 	 *
 	 * @return void
 	 */
-	public function pre_http_filter_debug(	string $url, array $arguments ): void {
+	public function pre_http_filter_debug( string $url, array $arguments ): void {
 		$message = sprintf( 'pre_http_request-filter-debug for %s', esc_url( $url ) );
 
 		$this->logger->info( $message, $arguments );
