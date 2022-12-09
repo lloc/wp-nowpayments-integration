62ad239b839ea1e538c21e85920f2e491695aed1
Dennis Ploetner <re@lloc.de>
2022-12-09 12:20:49 +0100

    Rollback

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import App from "./App";
-import { render } from '@wordpress/element';
-
-import 'semantic-ui-css/semantic.min.css';
-
-render(<App />, document.getElementById('nowpayments-integration') );
