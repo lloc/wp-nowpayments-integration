b92afc2e81c1ba465127cf1dfdbe7cd449fd60ae
Dennis Ploetner <re@lloc.de>
2024-12-02 09:09:23 +0100

    WordPress compatibility check for PHPStan installed

--- wp-nowpayments-integration.php
+++ wp-nowpayments-integration.php
@@ -8,6 +8,7 @@
  *
  * Plugin Name: WordPress nowpayments.io integration
  * Version: 1.0.0
+ * Requires at least: 6.6
  * Plugin URI: https://github.com/lloc/wp-nowpayments-itegration
  * Description: Cryptocurrency Payment integration using the NOWPayments API
  * Author: Dennis Ploetner
