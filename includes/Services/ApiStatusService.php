bfad0b5bd2cca105fffe28fe1843d50a1a339b78
Dennis Ploetner <re@lloc.de>
2024-12-08 09:25:38 +0100

    Bugfix

--- includes/Services/ApiStatusService.php
+++ includes/Services/ApiStatusService.php
@@ -6,7 +6,6 @@ use lloc\Nowpayments\Integration\ApiStatus;
 
 class ApiStatusService {
 
-
 	public function __construct(
 		private readonly ApiStatus $api_status
 	) { }
@@ -16,10 +15,9 @@ class ApiStatusService {
 	 */
 	public function get_data(): array {
 		$response = $this->api_status->get();
-		$result   = $response->get();
 
 		return array(
-			'status' => $result['status'] ?? 'ok',
+			'status' => $response->get()['message'] ?? 'ko',
 			'info'   => $this->api_status->get_client()->get_service()->info(),
 		);
 	}
