3d85dd63fdef380328b6c7472551506e01eb2244
Dennis Ploetner <re@lloc.de>
2025-12-02 11:43:07 +0100

    Branch Protection

--- bin/git-release.sh
+++ bin/git-release.sh
@@ -5,8 +5,8 @@ PLUGIN_NAME="wp-nowpayments-integration"
 BUILD_PATH="$PROJECT_ROOT/$PLUGIN_NAME"
 ZIP_ARCHIVE="$PROJECT_ROOT/$PLUGIN_NAME.zip"
 
-rm -f $ZIP_ARCHIVE
-rm -rf $BUILD_PATH && mkdir $BUILD_PATH
+rm -f "$ZIP_ARCHIVE"
+rm -rf "$BUILD_PATH" && mkdir "$BUILD_PATH"
 
-rsync -arvp --exclude-from=$PROJECT_ROOT/.distignore $PROJECT_ROOT/ $BUILD_PATH/
-cd $PROJECT_ROOT && zip -r $ZIP_ARCHIVE $PLUGIN_NAME
+rsync -arvp --exclude-from="$PROJECT_ROOT"/.distignore "$PROJECT_ROOT"/ "$BUILD_PATH"/
+cd "$PROJECT_ROOT" && zip -r "$ZIP_ARCHIVE" $PLUGIN_NAME
